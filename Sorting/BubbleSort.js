1ca420790a11114efe3852de31b173c47e416b55
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-02-02 11:04:21 +0300

    updated bubble sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,20 +1,12 @@
 function bubblesort(source) {
-    let left = 0;
-    let right = 1;
-    let temp = 0;
-    if (source.length == 0) {
-        return null
-    } else if (source.length == 1) {
+    if ((source.length == 0) || (source.length == 1)) {
         return source
     } else {
         for (let i = source.length; i > 0; i--) {
             for (let j = 0; j < i; j++) {
                 if (source[j] > source[j + 1]) {
-                    temp = source[j];
-                    source[j] = source[j + 1];
-                    source[j + 1] = temp;
+                    [source[j], source[j + 1]] = [source[j + 1], source[j]]
                 }
-
             }
         }
 
@@ -23,9 +15,7 @@ function bubblesort(source) {
 }
 
 
-function swap(left, right) {
-    return right, left;
-}
 
 
-console.log(bubblesort([]))
+
+console.log(bubblesort_stepwise([5, 4, 8, 7, 9, 2, 6]))
