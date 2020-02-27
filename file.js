b177452b3e0b4c9df868b3dc7b2af46f5b03696b
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-02-27 17:47:26 +0300

    updated csv importer

--- file.js
+++ file.js
@@ -1,7 +1,8 @@
 let fs = require('fs')
+var data = [];
 
 function readCSV(path) {
-    let data = [];
+
     let lineReader = require('readline').createInterface({
         input: require('fs').createReadStream(path)
     });
@@ -10,12 +11,17 @@ function readCSV(path) {
         line = line.split(",")
         line[0] = line[0].substr(1, line[0].length - 2)
         line[0] = line[0].replace('\t', '')
-        console.log(line);
+        line[0] = parseInt(line[0])
+        line[1] = parseInt(line[1])
+        data.push(line)
+            //console.log(line);
+
     });
-
-
+    console.log(data);
+    return data;
 }
 
 module.exports = {
     readCSV: readCSV,
+    data: data,
 }
