82d5be40e3f3ae2554edff653090a055c2811d37
Tesfamichael1074 <hope.miky1074@gmail.com>
2020-02-28 10:52:54 +0300

    variable scope bug

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-var data = [];
+let data = new Array(1, 5);
 
 function readCSV(path) {
 
@@ -8,20 +8,24 @@ function readCSV(path) {
     });
 
     lineReader.on('line', function(line) {
-        line = line.split(",")
-        line[0] = line[0].substr(1, line[0].length - 2)
-        line[0] = line[0].replace('\t', '')
-        line[0] = parseInt(line[0])
-        line[1] = parseInt(line[1])
-        data.push(line)
-            //console.log(line);
-
+        line = line.split(",");
+        line[0] = line[0].substr(1, line[0].length - 2);
+        line[0] = line[0].replace('\t', '');
+        line[0] = parseInt(line[0]);
+        line[1] = parseInt(line[1]);
+        appender(line);
+        //console.log(data);
     });
     console.log(data);
     return data;
 }
 
+function appender(tempdata) {
+
+    //console.log(data);
+    data.push(2);
+}
+
 module.exports = {
     readCSV: readCSV,
-    data: data,
 }
